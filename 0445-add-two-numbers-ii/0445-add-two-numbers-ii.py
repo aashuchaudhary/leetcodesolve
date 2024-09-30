163d4c3b531208d0d951122898ff54559f4ffb11
Ashutosh Chaudhary <123310232+aashuchaudhary@users.noreply.github.com>
2024-10-01 02:50:00 +0530

    Time: 49 ms (88.32%), Space: 16.6 MB (36.95%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -5,36 +5,18 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        def reverse(head):
-            prev=None
-            curr=head
-            while curr:
-                nxt=curr.next
-                curr.next=prev
-                prev=curr
-                curr=nxt
-            return prev
-        l1=reverse(l1)
-        l2=reverse(l2)
-        carry=0
-        dummy=ListNode()
-        temp=dummy
-        while l1 and l2:
-            s=l1.val+l2.val+carry
-            temp.next=ListNode(val=s%10)
-            carry=s//10
-            temp=temp.next
-            l1=l1.next
-            l2=l2.next
-        l=l1 if l1 else l2
-        while l:
-            s=l.val+carry
-            temp.next=ListNode(val=s%10)
-            carry=s//10
-            temp=temp.next
-            l=l.next
-        if carry>0: 
-            temp.next=ListNode(val=carry)
-            temp=temp.next
-        dummy=dummy.next
-        return reverse(dummy)
+        s1 = ''
+        s2 = ''
+        while l1:
+            s1+=str(l1.val)
+            l1 = l1.next
+        while l2:
+            s2+=str(l2.val)
+            l2 = l2.next    
+        tot = str(int(s1) +int(s2))
+
+        ans = itr = ListNode()
+        for i in tot:
+            itr.next= ListNode(i)
+            itr = itr.next  
+        return ans.next    
