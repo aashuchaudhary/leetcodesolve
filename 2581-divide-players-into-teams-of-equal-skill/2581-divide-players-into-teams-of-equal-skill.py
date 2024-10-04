48a0fed2c1d037df129c5ed94b89dd3f69224005
Ashutosh Chaudhary <123310232+aashuchaudhary@users.noreply.github.com>
2024-10-04 14:50:50 +0530

    Time: 403 ms (50.8%), Space: 29.6 MB (31.15%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.py
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.py
@@ -2,27 +2,16 @@ import collections
 
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
-        skill.sort()  # Sort to easily pair players
-        n = len(skill)
-        
-        group_count = n // 2
-        total_skill = sum(skill)
-        
-        # Ensure total skill is divisible by group count
-        if total_skill % group_count != 0:
-            return -1
-        
-        target_sum = total_skill // group_count  # Target sum for each pair
-        q = collections.deque(skill)
+        available = collections.defaultdict(int)
+        s = sum(skill) // (len(skill) // 2)
         chemistry = 0
 
-        for i in range(n//2):
-            a = skill[i]  # Smallest element
-            b = skill[n - i -1]      # Largest element
-
-            if a + b != target_sum:  # Check if the pair matches the target sum
-                return -1
-            
-            chemistry += a * b  # Accumulate the chemistry of the pair
-
+        for x in skill:
+            if available[s - x] > 0:
+                chemistry += x * (s - x)
+                available [s - x] -= 1
+            else :
+                available[x] += 1
+        if any(available[x] > 0 for x in available.keys()):
+            return -1
         return chemistry
