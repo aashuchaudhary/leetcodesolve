271ac69e110923a9721dff420d5159e2049dcbe0
Ashutosh Chaudhary <123310232+aashuchaudhary@users.noreply.github.com>
2024-10-06 11:43:31 +0530

    Time: 26 ms (74.71%), Space: 15.9 MB (94.21%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,43 +1,27 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int islands = 0;
-        int rows = grid.size();
-        int cols = grid[0].size();
-        unordered_set<string> visited;
-
-        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (grid[r][c] == '1' && visited.find(to_string(r) + "," + to_string(c)) == visited.end()) {
-                    islands++;
-                    bfs(grid, r, c, visited, directions, rows, cols);
+        int count=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j] == '1'){
+                    count++;
+                    dfs(grid,i,j);
                 }
             }
         }
-
-        return islands;        
+        return count;
     }
-
-private:
-    void bfs(vector<vector<char>>& grid, int r, int c, unordered_set<string>& visited, vector<pair<int, int>>& directions, int rows, int cols) {
-        queue<pair<int, int>> q;
-        visited.insert(to_string(r) + "," + to_string(c));
-        q.push({r, c});
-
-        while (!q.empty()) {
-            auto [row, col] = q.front();
-            q.pop();
-
-            for (auto [dr, dc] : directions) {
-                int nr = row + dr;
-                int nc = col + dc;
-                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == '1' && visited.find(to_string(nr) + "," + to_string(nc)) == visited.end()) {
-                    q.push({nr, nc});
-                    visited.insert(to_string(nr) + "," + to_string(nc));
-                }
-            }
-        }
+    void dfs(vector<vector<char>>& grid,int i,int j){
+        if(i<0 || j<0 || i == grid.size() || j == grid[0].size())
+            return;
+        if(grid[i][j] != '1')
+            return;
+        else
+            grid[i][j] = '2';
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
     }
-};    
+};
