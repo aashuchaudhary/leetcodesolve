45bc874bdc8773602a190a6834ae271d306125d6
Ashutosh Chaudhary <123310232+aashuchaudhary@users.noreply.github.com>
2024-10-07 12:43:25 +0530

    Time: 263 ms (97.45%), Space: 36.7 MB (52.85%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -4,34 +4,18 @@
 #         self.val = val
 #         self.next = next
 
-from typing import Optional
-
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        # Step 1: Use two pointers (fast and slow) to find the middle of the linked list
-        fast = head  # Fast pointer moves two steps at a time
-        slow = head  # Slow pointer moves one step at a time
-
-        # Traverse the list to reach the middle
-        while fast and fast.next:
-            fast = fast.next.next  # Fast pointer moves by 2 nodes
-            slow = slow.next  # Slow pointer moves by 1 node
-
-        # Step 2: Reverse the second half of the linked list
-        prev = None  # This will hold the reversed second half
-        while slow:
-            tmp = slow.next  # Temporarily store the next node
-            slow.next = prev  # Reverse the pointer of the current node
-            prev = slow  # Move prev pointer to the current node
-            slow = tmp  # Move to the next node in the original list
-
-        # Step 3: Compare the first half with the reversed second half
-        left, right = head, prev  # Left starts at the head, right starts at the beginning of the reversed half
-        while right:  # Only need to check the reversed half
-            if left.val != right.val:  # If the values don't match, it's not a palindrome
-                return False  # Return False if a mismatch is found
-            left = left.next  # Move left pointer forward
-            right = right.next  # Move right pointer forward (reversed second half)
-
-        # If all values matched, return True (it's a palindrome)
+        values = []
+        
+        # Store linked list values in a list
+        while head:
+            values.append(head.val)
+            head = head.next
+        
+        # Check if the list is a palindrome by comparing from both ends
+        for i in range(len(values) // 2):
+            if values[i] != values[-(i + 1)]:
+                return False
+        
         return True
