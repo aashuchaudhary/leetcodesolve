f39b8aa6c82bbda7717620a940ad9680154de1ad
Ashutosh Chaudhary <123310232+aashuchaudhary@users.noreply.github.com>
2024-10-07 12:39:31 +0530

    Time: 110 ms (93.18%), Space: 8.5 MB (85.3%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.go
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.go
@@ -6,21 +6,34 @@
  * }
  */
 func isPalindrome(head *ListNode) bool {
-    // Step 1: Convert linked list to slice
-    var nums []int
-    for head != nil {
-        nums = append(nums, head.Val)
-        head = head.Next
+    if head == nil || head.Next == nil {
+        return true
     }
 
-    // Step 2: Check if slice is a palindrome
-    left, right := 0, len(nums)-1
-    for left < right {
-        if nums[left] != nums[right] {
+    // Step 1: Find middle of the list (slow will point to the middle)
+    slow, fast := head, head
+    for fast != nil && fast.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+    }
+
+    // Step 2: Reverse the second half of the list
+    var prev *ListNode
+    for slow != nil {
+        temp := slow.Next
+        slow.Next = prev
+        prev = slow
+        slow = temp
+    }
+
+    // Step 3: Compare the first half with the reversed second half
+    left, right := head, prev
+    for right != nil {
+        if left.Val != right.Val {
             return false
         }
-        left++
-        right--
+        left = left.Next
+        right = right.Next
     }
 
     return true
