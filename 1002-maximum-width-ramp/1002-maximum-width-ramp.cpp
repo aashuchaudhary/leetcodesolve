c2cff54458b90ec1ee261f838a987a0fcec2814e
Ashutosh Chaudhary <123310232+aashuchaudhary@users.noreply.github.com>
2024-10-10 11:16:43 +0530

    Time: 58 ms (33.08%), Space: 47 MB (37.47%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         stack<int> s;
         
-        // Step 1: Build a decreasing stack of indices
+        // push element in decreasing order including elements of nums
         for (int i = 0; i < n; ++i) {
             if (s.empty() || nums[s.top()] > nums[i]) {
                 s.push(i);
@@ -14,8 +14,8 @@ public:
         int maxWidth = 0;
         
         // Step 2: Traverse from the end and find maximum width ramp
-        for (int j = n - 1; j >= 0; --j) {
-            while (!s.empty() && nums[s.top()] <= nums[j]) {
+        for (int j = n - 1; j >= 0; --j) {  //itertae from right to left
+            while (!s.empty() && nums[s.top()] <= nums[j]) {  //check stack is not empyu
                 maxWidth = max(maxWidth, j - s.top());
                 s.pop();
             }
@@ -24,3 +24,12 @@ public:
         return maxWidth;
     }
 };
+
+
+// NOTE: width of such a ramp is index: (j - i).
+// nums is a random pair (i, j) for which i < j 
+// and    nums[i] <= nums[j]
+
+// eg; nums = [6,0,8,2,1,5]
+// index   :   0,1,2,3,4,5
+//  here the max width isin case of the  index 6-1,here we xheck the pair of elements in which we select the random element i and search for the the larget elemnt than the element present in the ith position and the the max other than i named as j and the the ax width will be (j-1).
