08aa0692b11df99b8429c831aa76f7b6900ffe5d
Ashutosh Chaudhary <123310232+aashuchaudhary@users.noreply.github.com>
2024-10-15 06:59:38 +0530

    Time: 70 ms (94.03%), Space: 16.9 MB (10.13%) - LeetHub

---
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.py
@@ -0,0 +1,26 @@
+class Solution:
+    def subArrayRanges(self, nums: List[int]) -> int:
+        res = 0
+        inf = float('inf')
+        
+        # Handling maximums
+        A = [-inf] + nums + [-inf]
+        s = []
+        for i, x in enumerate(A):
+            while s and A[s[-1]] > x:
+                j = s.pop()
+                k = s[-1]
+                res -= A[j] * (i - j) * (j - k)
+            s.append(i)
+        
+        # Handling minimums
+        A = [inf] + nums + [inf]
+        s = []
+        for i, x in enumerate(A):
+            while s and A[s[-1]] < x:
+                j = s.pop()
+                k = s[-1]
+                res += A[j] * (i - j) * (j - k)
+            s.append(i)
+        
+        return res
