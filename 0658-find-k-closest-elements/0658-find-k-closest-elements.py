15222c5beacf033ab841d52293b3c529404843a0
Ashutosh Chaudhary <123310232+aashuchaudhary@users.noreply.github.com>
2024-10-19 20:58:10 +0530

    Time: 11 ms (100%), Space: 17.7 MB (99.63%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,4 +1,28 @@
 class Solution:
     def findClosestElements(self, arr, k, x):
-        arr.sort(key=lambda num: (abs(num - x), num))
-        return sorted(arr[:k])
+        left, right = 0, len(arr) - 1
+        
+        # Find the position to insert x to maintain order
+        while left < right:
+            mid = left + (right - left) // 2
+            if arr[mid] < x:
+                left = mid + 1
+            else:
+                right = mid
+        
+        # Now, left is the insertion point
+        start, end = left - 1, left
+        
+        while k > 0:
+            if start < 0:
+                end += 1
+            elif end >= len(arr):
+                start -= 1
+            elif abs(arr[start] - x) <= abs(arr[end] - x):
+                start -= 1
+            else:
+                end += 1
+            
+            k -= 1
+        
+        return sorted(arr[start + 1:end])
