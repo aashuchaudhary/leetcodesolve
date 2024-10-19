c5f13ff7d912249b694cc7b1e40c71a59dfd03ad
Ashutosh Chaudhary <123310232+aashuchaudhary@users.noreply.github.com>
2024-10-19 20:54:10 +0530

    Time: 47 ms (32.16%), Space: 38.1 MB (21.33%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-             int left = 0;
-        int right = arr.size() - 1;
-        
-        // Reduce the range to exactly k elements
-        while (right - left >= k) {
-            // Compare distances of left and right to x
-            if (abs(arr[left] - x) > abs(arr[right] - x)) {
-                left++;  // Move left closer to the right
-            } else {
-                right--;  // Move right closer to the left
+               // Create a max-heap to keep track of the k closest elements
+        priority_queue<pair<int, int>> maxHeap;
+
+        for (int num : arr) {
+            // Calculate the distance from x
+            int distance = abs(num - x);
+            // Push the distance and number onto the heap
+            maxHeap.push({distance, num});
+            
+            // If the heap size exceeds k, pop the farthest element
+            if (maxHeap.size() > k) {
+                maxHeap.pop();
             }
         }
-        
-        // Return the k closest elements
-        return vector<int>(arr.begin() + left, arr.begin() + left + k);
+
+        // Extract the k closest elements from the heap
+        vector<int> result;
+        while (!maxHeap.empty()) {
+            result.push_back(maxHeap.top().second);
+            maxHeap.pop();
+        }
+
+        // Sort the result before returning
+        sort(result.begin(), result.end());
+        return result;
     }
 };
