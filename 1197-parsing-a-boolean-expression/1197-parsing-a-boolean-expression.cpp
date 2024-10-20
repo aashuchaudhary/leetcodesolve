be7cd7212fd11a8ea0fbccde93048d734713ddef
Ashutosh Chaudhary <123310232+aashuchaudhary@users.noreply.github.com>
2024-10-20 22:16:11 +0530

    Time: 2 ms (95.24%), Space: 9.8 MB (64.32%) - LeetHub

--- 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.cpp
+++ 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.cpp
@@ -1,52 +1,35 @@
 class Solution {
-    void evaluate(std::stack<char>& optr, std::stack<char>& operand) {
-        char op = optr.top();  // Get the character at the top
-        optr.pop();  // Remove the operator from the stack
-        bool ans;
-
-        if (op == '|') {
-            ans = false;  // For OR, initial value should be false
-            while (operand.top() != '(') {
-                ans |= (operand.top() == 't');
-                operand.pop();
-            }
-            operand.pop();  // Pop the '('
-        } else if (op == '&') {
-            ans = true;  // For AND, initial value should be true
-            while (operand.top() != '(') {
-                ans &= (operand.top() == 't');
-                operand.pop();
-            }
-            operand.pop();  // Pop the '('
-        } else if (op == '!') {
-            ans = (operand.top() == 't');
-            ans = !ans;  // Negate the value
-            operand.pop();  // Pop the value
-            operand.pop();  // Pop the '('
-        }
-
-        if (ans) operand.push('t');  // Push true result back to operand stack
-        else operand.push('f');  // Push false result back to operand stack
-    }
-
 public:
-    bool parseBoolExpr(std::string expression) {
-        std::stack<char> optr, operand;
-        int idx = 0;
+    bool parseBoolExpr(string expression) {
+        stack<char> st;
 
-        while (expression[idx]) {
-            if (expression[idx] == '!' || expression[idx] == '&' || expression[idx] == '|') {
-                optr.push(expression[idx]);
-            } else if (expression[idx] == 't' || expression[idx] == 'f') {
-                operand.push(expression[idx]);  // Push operand
-            } else if (expression[idx] == '(') {
-                operand.push(expression[idx]);  // Pushing opening parenthesis
-            } else if (expression[idx] == ')') {
-                evaluate(optr, operand);  // Evaluate when closing parenthesis is encountered
+        for (char currChar : expression) {
+            if (currChar == ',' || currChar == '(')
+                continue;  
+            if (currChar == 't' || currChar == 'f' || currChar == '!' ||
+                currChar == '&' || currChar == '|') {
+                st.push(currChar);
             }
-            idx++;
-        }
+            else if (currChar == ')') {
+                bool hasTrue = false, hasFalse = false;
 
-        return operand.top() == 't';  // Final result check
+                while (st.top() != '!' && st.top() != '&' && st.top() != '|') {
+                    char topValue = st.top();
+                    st.pop();
+                    if (topValue == 't') hasTrue = true;
+                    if (topValue == 'f') hasFalse = true;
+                }
+                char op = st.top();
+                st.pop();
+                if (op == '!') {
+                    st.push(hasTrue ? 'f' : 't');
+                } else if (op == '&') {
+                    st.push(hasFalse ? 'f' : 't');
+                } else {
+                    st.push(hasTrue ? 't' : 'f');
+                }
+            }
+        }
+        return st.top() == 't';
     }
 };
